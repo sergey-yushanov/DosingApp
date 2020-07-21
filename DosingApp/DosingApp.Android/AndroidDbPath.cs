103fec7a939ee5e55c45b0d7d3192703897ce3a8
yushanov <sergeyyushanov@gmail.com>
2020-07-21 19:26:48 +0400

    add db context

---
+++ DosingApp/DosingApp.Android/AndroidDbPath.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using DosingApp.Droid;
+using DosingApp.Services;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AndroidDbPath))]
+namespace DosingApp.Droid
+{
+    public class AndroidDbPath : IPath
+    {
+        public string GetDatabasePath(string filename)
+        {
+            return Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), filename);
+        }
+    }
+}
---
+++ DosingApp/DosingApp.UWP/UwpDbPath.cs
@@ -0,0 +1,17 @@
+﻿using DosingApp.Services;
+using DosingApp.UWP;
+using System.IO;
+using Windows.Storage;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(UwpDbPath))]
+namespace DosingApp.UWP
+{
+    public class UwpDbPath : IPath
+    {
+        public string GetDatabasePath(string filename)
+        {
+            return Path.Combine(ApplicationData.Current.LocalFolder.Path, filename);
+        }
+    }
+}

2887e21c6205933d385ee7274973e3e1c2bfdc0c
yushanov <sergeyyushanov@gmail.com>
2020-07-29 15:27:40 +0400

    return init case for field pages

--- DosingApp/DosingApp/Views/FieldPage.xaml.cs
+++ DosingApp/DosingApp/Views/FieldPage.xaml.cs
@@ -1,13 +1,60 @@
-﻿using DosingApp.Models;
+﻿using DosingApp.DataContext;
+using DosingApp.Models;
+using DosingApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace DosingApp.Views
 {
     public partial class FieldPage : ContentPage
     {
+        string dbPath;
+
         public FieldPage()
         {
             InitializeComponent();
+            dbPath = DependencyService.Get<IPath>().GetDatabasePath(App.DBFILENAME);
+        }
+
+        private void Back()
+        {
+            this.Navigation.PopAsync();
+        }
+
+        private void SaveButton(object sender, EventArgs e)
+        {
+            var field = (Field)BindingContext;
+            if (!String.IsNullOrEmpty(field.Name))
+            {
+                using (AppDbContext db = new AppDbContext(dbPath))
+                {
+                    if (field.FieldId == 0)
+                        db.Fields.Add(field);
+                    else
+                    {
+                        db.Fields.Update(field);
+                    }
+                    db.SaveChanges();
+                }
+            }
+            Back();
+        }
+
+        private void DeleteButton(object sender, EventArgs e)
+        {
+            var field = (Field)BindingContext;
+            using (AppDbContext db = new AppDbContext(dbPath))
+            {
+                db.Fields.Remove(field);
+                db.SaveChanges();
+            }
+            Back();
         }
     }
 }
--- DosingApp/DosingApp/Views/FieldsPage.xaml.cs
+++ DosingApp/DosingApp/Views/FieldsPage.xaml.cs
@@ -1,5 +1,14 @@
-﻿using DosingApp.ViewModels;
+﻿using DosingApp.DataContext;
+using DosingApp.Models;
+using DosingApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace DosingApp.Views
 {
@@ -8,14 +17,34 @@ namespace DosingApp.Views
         public FieldsPage()
         {
             InitializeComponent();
-            //BindingContext = new FieldsViewModel();
         }
 
         protected override void OnAppearing()
         {
-            FieldsViewModel fieldsViewModel = (FieldsViewModel)this.BindingContext;
-            fieldsViewModel.LoadFields();
+            string dbPath = DependencyService.Get<IPath>().GetDatabasePath(App.DBFILENAME);
+            using (AppDbContext db = new AppDbContext(dbPath))
+            {
+                itemsList.ItemsSource = db.Fields.ToList();
+            }
             base.OnAppearing();
+        }
+
+        // обработка нажатия элемента в списке
+        private async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Field selectedField = (Field)e.SelectedItem;
+            FieldPage fieldPage = new FieldPage();
+            fieldPage.BindingContext = selectedField;
+            await Navigation.PushAsync(fieldPage);
+        }
+
+        // обработка нажатия кнопки добавления
+        private async void CreateButton(object sender, EventArgs e)
+        {
+            Field field = new Field();
+            FieldPage fieldPage = new FieldPage();
+            fieldPage.BindingContext = field;
+            await Navigation.PushAsync(fieldPage);
         }
     }
 }

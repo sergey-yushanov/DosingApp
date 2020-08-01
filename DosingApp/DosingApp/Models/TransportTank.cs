d29445ae5f23df96a2fd38092a15763022d4cf21
yushanov <sergeyyushanov@gmail.com>
2020-08-01 17:29:03 +0400

    add model transport tank

---
+++ DosingApp/DosingApp/Models/TransportTank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DosingApp.Models
+{
+    public class TransportTank
+    {
+        public int TransportTankId { get; set; }
+        public string Name { get; set; }
+        public float Volume { get; set; }
+
+        public int? TransportId { get; set; }
+        public virtual Transport Transport { get; set; }
+    }
+}

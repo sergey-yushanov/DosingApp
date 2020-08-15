51c6af143ddf6019775f168f6c242bc4fc0f320a
yushanov <sergeyyushanov@gmail.com>
2020-08-15 14:40:12 +0400

    add mixer model

---
+++ DosingApp/DosingApp/Models/Mixer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DosingApp.Models
+{
+    public class Mixer
+    {
+        public int MixerId { get; set; }
+        public string Name { get; set; }
+
+        public int Collector { get; set; }
+        public int Single { get; set; }
+        public int ThreeWayValve { get; set; }
+    }
+}

12e425255604f0553c92ed346a7c3364644f014f
yushanov <sergeyyushanov@gmail.com>
2020-08-15 14:12:20 +0400

    little changes to interface

--- DosingApp/DosingApp/Models/User.cs
+++ DosingApp/DosingApp/Models/User.cs
@@ -8,7 +8,6 @@ namespace DosingApp.Models
     public class User
     {
         public int UserId { get; set; }
-        //[Index(IsUnique = true)]
         public string Username { get; set; }
         public string DisplayName { get; set; }
         public byte[] PasswordSalt { get; set; }

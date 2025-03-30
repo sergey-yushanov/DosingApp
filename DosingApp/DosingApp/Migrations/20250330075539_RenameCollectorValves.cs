f1c0d360e5313306cb49a739d9a38340f117e23b
Sergey Yushanov <ysv@rusatom.dev>
2025-03-30 12:14:14 +0300

    Изменены названия клапанов коллекторов

---
+++ DosingApp/DosingApp/Migrations/20250330075539_RenameCollectorValves.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DosingApp.Migrations
+{
+    public partial class RenameCollectorValves : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE JobComponents SET Dispenser = (substr(Dispenser, 1, 1) || 'К' || substr(Dispenser, 5, 1) || 'Кл') WHERE Dispenser LIKE '%Кол%';\n" +
+                "UPDATE RecipeComponents SET Dispenser = (substr(Dispenser, 1, 1) || 'К' || substr(Dispenser, 5, 1) || 'Кл') WHERE Dispenser LIKE '%Кол%';\n" +
+                "UPDATE ReportComponents SET Dispenser = (substr(Dispenser, 1, 1) || 'К' || substr(Dispenser, 5, 1) || 'Кл') WHERE Dispenser LIKE '%Кол%';"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
